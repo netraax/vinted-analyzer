51317be4cc39ea01226db68e9b461cd532ca9c27
netraax <dils389@gmail.com>
2024-11-30 18:35:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/.netlify': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+      }
+    }
   }
 })
